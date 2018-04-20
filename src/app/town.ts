e005dad17325e07a5ac90d9dd218000c92ea9e0f
math.puget <math.puget@gmail.com>
2018-04-20 11:41:02 -0400

    cool

--- src/app/town.ts
+++ src/app/town.ts
@@ -1,16 +1,6 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { Town } from './town';
-
-@Injectable()
-export class TownService {
- 
-  private URL = "http://lpa2sgadot.herokuapp.com/towns"
-
-  constructor(private http: HttpClient) { }
-
-  getTown(id : number) : Observable<Town> {
-    return this.http.get<Town>(`${this.URL}/${id}.json`);
-  }
+export class Town {
+  id: number;
+  name: string;
+  icon: string;
+  temperature: number;
 }
